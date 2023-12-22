9af1aaff8b01aea91017a49b67af1a72b788e9e3
truth-dev <Truth8510@hotmail.com>
2023-12-21 19:45:05 -0700

    finished guided

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,32 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../../config')
+
+
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization // this is the cookie we set in the login endpoint
+  if(token){
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if(err){
+        next({status: 401, message: `token bad, you cant come in`})
+      }else{
+      req.decodedJwt = decoded
+      console.log(req.decodedJwt)  
+      }
+  }) 
+}else{
+    next({status: 401, message: 'wtf, no token?!'})
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+  if(req.decodedJwt && req.decodedJwt.role === role){
+    next()
+  }else{
+    next({status: 403, message:'you have no power here'})
+  }
 }
 
 module.exports = {
